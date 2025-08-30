84b08f0926b98c50e7ff00bd236f3cad1fb45b24
Harvey <rhw15020306490@163.com>
2025-08-30 16:29:40 +0800

    commit

---
+++ src/main/java/me/maple_bamboo_team/autoelytra/Autoelytra.java
@@ -0,0 +1,10 @@
+package me.maple_bamboo_team.autoelytra;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Autoelytra implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/maple_bamboo_team/autoelytra/client/AutoelytraClient.java
@@ -0,0 +1,10 @@
+package me.maple_bamboo_team.autoelytra.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoelytraClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
